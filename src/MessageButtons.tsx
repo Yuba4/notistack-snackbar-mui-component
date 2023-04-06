9e001872e7cf78d935485868f7dabc3f2cd6894a
Hiroto <abyu629@gmail.com>
2023-04-07 01:13:58 +0900

    MessageButtonのstyleなどを変更

--- src/MessageButtons.tsx
+++ src/MessageButtons.tsx
@@ -1,4 +1,4 @@
-import { useCallback, Fragment } from "react";
+import { useCallback, ReactNode } from "react";
 import Button from "@mui/material/Button";
 import Paper from "@mui/material/Paper";
 import { AlertColor } from "@mui/material";
@@ -6,13 +6,6 @@ import { AlertColor } from "@mui/material";
 import { useMuiSnackbar } from "./hooks/useMuiSnackbar";
 
 const styles = {
-  root: {
-    flexGrow: 1,
-    display: "flex",
-    margin: 16,
-    justifyContent: "center",
-    alignItems: "middle",
-  },
   button: {
     margin: 8,
     borderColor: "#313131",
@@ -23,78 +16,74 @@ const styles = {
     color: "#43a047",
   },
   error: {
-    borderColor: "#d32f2f",
-    color: "#d32f2f",
+    borderColor: "#f44336",
+    color: "#f44336",
   },
   info: {
-    borderColor: "#2979ff",
-    color: "#2979ff",
+    borderColor: "#2196f3",
+    color: "#2196f3",
   },
   warning: {
-    borderColor: "#ffa000",
-    color: "#ffa000",
+    borderColor: "#ff9800",
+    color: "#ff9800",
   },
 };
 
-const buttons: { variant: AlertColor; message: string }[] = [
-  { variant: "success", message: "Successfully done the operation." },
-  { variant: "error", message: "Something went wrong." },
-  { variant: "warning", message: "Something could go wrong" },
-  { variant: "info", message: "For your info..." },
-];
-
 const MessageButtons = () => {
-  const { enqueueSnackbar, closeSnackbar } = useMuiSnackbar();
+  const { enqueueSnackbar, closeSnackbar } = useMuiSnackbar({});
+
+  const buttons: {
+    variant: AlertColor;
+    message: string;
+    action?: ReactNode;
+  }[] = [
+    { variant: "success", message: "This is a success message!" },
+    { variant: "error", message: "This is an error message!" },
+    { variant: "warning", message: "This is a warning message!" },
+    {
+      variant: "info",
+      message: "This is an information message!",
+      action: (
+        <Button onClick={() => closeSnackbar()} color="inherit" size="small">
+          UNDO
+        </Button>
+      ),
+    },
+  ];
 
   const handleClick = useCallback(
-    ({ variant, message }: { variant: AlertColor; message: string }) =>
+    ({
+        variant,
+        message,
+        action,
+      }: {
+        variant: AlertColor;
+        message: string;
+        action: ReactNode;
+      }) =>
       () => {
-        enqueueSnackbar(message, { variant });
+        enqueueSnackbar(message, { variant, action });
       },
     [enqueueSnackbar]
   );
 
-  const handleClickWithAction = useCallback(() => {
-    enqueueSnackbar("I use snackbars responsibly", {
-      variant: "default",
-      action: (key) => (
-        <Fragment>
-          <Button
-            size="large"
-            onClick={() =>
-              alert(`Clicked on action of snackbar with id: ${key}`)
-            }
-          >
-            Detail
-          </Button>
-          <Button size="small" onClick={() => closeSnackbar(key)}>
-            Dismiss
-          </Button>
-        </Fragment>
-      ),
-    });
-  }, [enqueueSnackbar, closeSnackbar]);
-
   return (
-    <Paper style={styles.root}>
+    <div>
       {buttons.map((button) => (
         <Button
           key={button.variant}
           variant="outlined"
-          style={{ ...styles.button }}
-          onClick={handleClick(button)}
+          style={{ ...styles.button, ...styles[button.variant] }}
+          onClick={handleClick({
+            action: button.action,
+            message: button.message,
+            variant: button.variant,
+          })}
         >
           {button.variant}
         </Button>
       ))}
-      <Button
-        variant="outlined"
-        style={styles.button}
-        onClick={handleClickWithAction}
-      >
-        default
-      </Button>
-    </Paper>
+    </div>
   );
 };
 
